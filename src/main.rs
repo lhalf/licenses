e103ca7deb571d9f22a2b62250e6a4d894615bc8
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-05-09 14:41:11 +0100

    add initial find and copy licenses

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,28 @@ fn main() -> Result<(), anyhow::Error> {
     dbg!(dependencies);
     Ok(())
 }
+
+trait FileSystem {}
+
+fn find_and_copy_licenses(_crate_directories: Vec<()>, _filesystem: &impl FileSystem) {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Default)]
+    struct FileSystemSpy {
+        files_copied: Vec<()>,
+    }
+
+    impl FileSystem for FileSystemSpy {}
+
+    #[test]
+    fn no_dependencies_copies_no_licenses() {
+        let file_system_spy = FileSystemSpy::default();
+
+        find_and_copy_licenses(Vec::new(), &file_system_spy);
+
+        assert!(file_system_spy.files_copied.is_empty())
+    }
+}
