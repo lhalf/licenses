dae35b300ba046b93754e9c419d930b844b13731
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-05-10 21:43:48 +0100

    copy single license if crate directory contains one license

--- src/file_system.rs
+++ src/file_system.rs
@@ -1,10 +1,20 @@
-pub trait FileSystem {}
+use std::{cell::RefCell, path::Path};
+
+pub trait FileSystem {
+    fn copy_file(&self, from: &Path, to: &Path);
+}
 
 #[cfg(test)]
 #[derive(Default)]
 pub struct FileSystemSpy {
-    pub files_copied: Vec<()>,
+    pub files_copied: RefCell<Vec<String>>,
 }
 
 #[cfg(test)]
-impl FileSystem for FileSystemSpy {}
+impl FileSystem for FileSystemSpy {
+    fn copy_file(&self, from: &Path, _to: &Path) {
+        self.files_copied
+            .borrow_mut()
+            .push(from.to_string_lossy().to_string());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 mod file_system;
 
+use std::path::PathBuf;
+
 use file_system::FileSystem;
 
 fn main() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-trait CrateDirectory {}
+trait CrateDirectory {
+    fn get_license(&self) -> Option<PathBuf>;
+}
 
 fn find_and_copy_licenses<C: CrateDirectory>(
-    _crate_directories: Vec<C>,
-    _filesystem: &impl FileSystem,
+    crate_directories: Vec<C>,
+    filesystem: &impl FileSystem,
 ) {
+    for crate_directory in crate_directories {
+        if let Some(license_path) = crate_directory.get_license() {
+            filesystem.copy_file(&license_path, &PathBuf::new());
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use super::find_and_copy_licenses;
     use crate::{CrateDirectory, file_system::FileSystemSpy};
 
@@ -25,21 +36,44 @@ mod tests {
 
         find_and_copy_licenses(Vec::<CrateDirectoryFake>::new(), &file_system_spy);
 
-        assert!(file_system_spy.files_copied.is_empty())
+        assert!(file_system_spy.files_copied.take().is_empty())
     }
 
-    #[derive(Default)]
-    struct CrateDirectoryFake {}
+    struct CrateDirectoryFake {
+        license: Option<String>,
+    }
 
-    impl CrateDirectory for CrateDirectoryFake {}
+    impl CrateDirectoryFake {
+        fn containing_license(license_name: Option<&str>) -> Self {
+            Self {
+                license: license_name.map(|license_name| license_name.to_string()),
+            }
+        }
+    }
+
+    impl CrateDirectory for CrateDirectoryFake {
+        fn get_license(&self) -> Option<PathBuf> {
+            self.license.as_ref().map(|license| PathBuf::from(license))
+        }
+    }
 
     #[test]
     fn when_there_is_one_crate_with_no_licenses_then_no_license_files_are_copied() {
         let file_system_spy = FileSystemSpy::default();
-        let crate_directory_fake = CrateDirectoryFake::default();
+        let crate_directory_fake = CrateDirectoryFake::containing_license(None);
 
         find_and_copy_licenses(vec![crate_directory_fake], &file_system_spy);
 
-        assert!(file_system_spy.files_copied.is_empty())
+        assert!(file_system_spy.files_copied.take().is_empty())
+    }
+
+    #[test]
+    fn when_there_is_one_crate_with_one_license_then_one_license_file_copied() {
+        let file_system_spy = FileSystemSpy::default();
+        let crate_directory_fake = CrateDirectoryFake::containing_license(Some("LICENSE-MIT"));
+
+        find_and_copy_licenses(vec![crate_directory_fake], &file_system_spy);
+
+        assert_eq!(vec!["LICENSE-MIT"], file_system_spy.files_copied.take())
     }
 }
