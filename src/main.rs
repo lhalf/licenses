98a6faa08e69b19e519822493438bd61ea077912
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-05-10 21:21:37 +0100

    add initial crate directory trait

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,39 @@ fn main() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
-fn find_and_copy_licenses(_crate_directories: Vec<()>, _filesystem: &impl FileSystem) {}
+trait CrateDirectory {}
+
+fn find_and_copy_licenses<C: CrateDirectory>(
+    _crate_directories: Vec<C>,
+    _filesystem: &impl FileSystem,
+) {
+}
 
 #[cfg(test)]
 mod tests {
     use super::find_and_copy_licenses;
-    use crate::file_system::FileSystemSpy;
+    use crate::{CrateDirectory, file_system::FileSystemSpy};
 
     #[test]
     fn when_there_are_no_crates_no_license_files_are_copied() {
         let file_system_spy = FileSystemSpy::default();
 
-        find_and_copy_licenses(Vec::new(), &file_system_spy);
+        find_and_copy_licenses(Vec::<CrateDirectoryFake>::new(), &file_system_spy);
+
+        assert!(file_system_spy.files_copied.is_empty())
+    }
+
+    #[derive(Default)]
+    struct CrateDirectoryFake {}
+
+    impl CrateDirectory for CrateDirectoryFake {}
+
+    #[test]
+    fn when_there_is_one_crate_with_no_licenses_then_no_license_files_are_copied() {
+        let file_system_spy = FileSystemSpy::default();
+        let crate_directory_fake = CrateDirectoryFake::default();
+
+        find_and_copy_licenses(vec![crate_directory_fake], &file_system_spy);
 
         assert!(file_system_spy.files_copied.is_empty())
     }
