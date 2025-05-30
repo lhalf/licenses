aadef7f2710a45e7d6e1627aea782c7ceb851966
benaiah-pa <benaiah.mutinda@paconsulting.com>
2025-05-30 14:59:08 +0100

    Added test for license path

--- src/find_licenses.rs
+++ src/find_licenses.rs
@@ -4,12 +4,18 @@ pub trait FindLicenses {
     fn find_licenses(&self) -> Result<Vec<PathBuf>, anyhow::Error>;
 }
 
-trait DirEntry {}
+trait DirEntry {
+    fn filename(&self) -> String;
+    fn path(&self) -> PathBuf;
+}
 
 fn find_licenses_in_directory(
-    _dir_entries: impl Iterator<Item = impl DirEntry>,
+    dir_entries: impl Iterator<Item = impl DirEntry>,
 ) -> Result<Vec<PathBuf>, anyhow::Error> {
-    Ok(Vec::new())
+    Ok(dir_entries
+        .filter(|dir_entry| dir_entry.filename().contains("license"))
+        .map(|dir_entry| dir_entry.path())
+        .collect())
 }
 
 #[cfg(test)]
@@ -50,8 +56,19 @@ impl FindLicenses for CrateDirectoryFake {
 mod tests {
     use super::PathBuf;
     use super::{DirEntry, find_licenses_in_directory};
-    pub struct DirEntryFake {}
-    impl DirEntry for DirEntryFake {}
+    pub struct DirEntryFake {
+        pub filename: &'static str,
+        pub path: PathBuf,
+    }
+    impl DirEntry for DirEntryFake {
+        fn filename(&self) -> String {
+            self.filename.to_string()
+        }
+
+        fn path(&self) -> PathBuf {
+            self.path.clone()
+        }
+    }
     #[test]
     fn empty_crate_directory_has_no_licences() {
         assert_eq!(
@@ -59,12 +76,34 @@ mod tests {
             find_licenses_in_directory(Vec::<DirEntryFake>::new().into_iter()).unwrap()
         )
     }
-    
+
     #[test]
     fn crate_directory_with_no_licences() {
         assert_eq!(
             Vec::<PathBuf>::new(),
-            find_licenses_in_directory([DirEntryFake{}].into_iter()).unwrap()
+            find_licenses_in_directory(
+                [DirEntryFake {
+                    filename: "file.txt",
+                    path: PathBuf::new()
+                }]
+                .into_iter()
+            )
+            .unwrap()
+        )
+    }
+
+    #[test]
+    fn crate_directory_with_one_licences_returns_path_for_licence() {
+        assert_eq!(
+            vec![PathBuf::from("path/license")],
+            find_licenses_in_directory(
+                [DirEntryFake {
+                    filename: "license",
+                    path: PathBuf::from("path/license")
+                }]
+                .into_iter()
+            )
+            .unwrap()
         )
     }
 }
