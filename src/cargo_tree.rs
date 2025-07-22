73e38de6d98c399c01f5825da00bac69b2c63956
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-07-22 21:21:00 +0100

    allow setting no-dev and no-build args in cargo tree output

--- src/cargo_tree.rs
+++ src/cargo_tree.rs
@@ -1,8 +1,72 @@
 use anyhow::Context;
 use itertools::Itertools;
 use std::collections::BTreeSet;
+use std::process::Command;
 
-pub fn to_crate_names(output: Vec<u8>) -> anyhow::Result<BTreeSet<String>> {
+pub fn crate_names(
+    depth: Option<u8>,
+    include_dev_dependencies: bool,
+    include_build_dependencies: bool,
+    excluded_workspaces: Vec<String>,
+) -> anyhow::Result<BTreeSet<String>> {
+    to_crate_names(
+        Command::new("cargo")
+            .args(args(
+                depth,
+                include_dev_dependencies,
+                include_build_dependencies,
+                excluded_workspaces,
+            ))
+            .output()
+            .context("failed to call cargo tree")?
+            .stdout,
+    )
+}
+
+fn args(
+    depth: Option<u8>,
+    include_dev_dependencies: bool,
+    include_build_dependencies: bool,
+    excluded_workspaces: Vec<String>,
+) -> Vec<String> {
+    let mut args = vec![
+        "tree".to_string(),
+        "--format".to_string(),
+        "{lib}".to_string(),
+        "--prefix".to_string(),
+        "none".to_string(),
+        "--no-dedupe".to_string(),
+    ];
+
+    let mut edges = Vec::new();
+    if !include_dev_dependencies {
+        edges.push("no-dev");
+    }
+    if !include_build_dependencies {
+        edges.push("no-build");
+    }
+    if !edges.is_empty() {
+        args.push("--edges".to_string());
+        args.push(edges.join(","));
+    }
+
+    if let Some(depth) = depth {
+        args.push("--depth".to_string());
+        args.push(depth.to_string());
+    }
+
+    if !excluded_workspaces.is_empty() {
+        args.push("--workspace".to_string());
+        excluded_workspaces.into_iter().for_each(|workspace| {
+            args.push("--exclude".to_string());
+            args.push(workspace);
+        });
+    };
+
+    args
+}
+
+fn to_crate_names(output: Vec<u8>) -> anyhow::Result<BTreeSet<String>> {
     Ok(String::from_utf8(output)
         .context("cargo tree output contained invalid UTF-8")?
         .replace(" ", "")
@@ -15,8 +79,50 @@ pub fn to_crate_names(output: Vec<u8>) -> anyhow::Result<BTreeSet<String>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::cargo_tree::to_crate_names;
+    use crate::cargo_tree::{args, to_crate_names};
     use std::collections::BTreeSet;
+    
+    #[test]
+    fn default_args() {
+        assert_eq!(vec![
+            "tree".to_string(),
+            "--format".to_string(),
+            "{lib}".to_string(),
+            "--prefix".to_string(),
+            "none".to_string(),
+            "--no-dedupe".to_string(),
+            "--edges".to_string(),
+            "no-dev,no-build".to_string(),
+        ], args(None, false, false, vec![]));
+    }
+
+    #[test]
+    fn include_dev_dependencies_args() {
+        assert_eq!(vec![
+            "tree".to_string(),
+            "--format".to_string(),
+            "{lib}".to_string(),
+            "--prefix".to_string(),
+            "none".to_string(),
+            "--no-dedupe".to_string(),
+            "--edges".to_string(),
+            "no-build".to_string(),
+        ], args(None, true, false, vec![]));
+    }
+
+    #[test]
+    fn include_build_dependencies_args() {
+        assert_eq!(vec![
+            "tree".to_string(),
+            "--format".to_string(),
+            "{lib}".to_string(),
+            "--prefix".to_string(),
+            "none".to_string(),
+            "--no-dedupe".to_string(),
+            "--edges".to_string(),
+            "no-dev".to_string(),
+        ], args(None, false, true, vec![]));
+    }
 
     #[test]
     fn invalid_utf8_in_cargo_tree_output() {
