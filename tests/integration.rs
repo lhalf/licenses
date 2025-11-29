ea7b435a8f41639b444888dc206a48b6c0037e57
Laurence Halfpenny <80602159+lhalf@users.noreply.github.com>
2025-11-29 19:25:39 +0000

    add integration test for collect depth 1

--- tests/integration.rs
+++ tests/integration.rs
@@ -9,6 +9,19 @@ fn call_licenses_command(args: &[&str]) -> std::process::Output {
         .unwrap()
 }
 
+fn list_files(dir: &std::path::Path) -> Vec<String> {
+    std::fs::read_dir(dir)
+        .unwrap()
+        .filter_map(|entry| entry.ok())
+        .map(|entry| entry.path())
+        .filter_map(|path| {
+            path.file_name()
+                .and_then(|name| name.to_str())
+                .map(|name| name.to_string())
+        })
+        .collect()
+}
+
 #[test]
 fn help_flag() {
     let output = call_licenses_command(&["--help"]);
@@ -17,4 +30,44 @@ fn help_flag() {
         include_str!("stdout/help"),
         String::from_utf8(output.stdout).unwrap()
     )
+}
+
+#[test]
+fn collect_depth_1() {
+    let temp_dir = tempfile::TempDir::new().unwrap();
+    let temp_dir_path = temp_dir.path();
+
+    let output = call_licenses_command(&[
+        "collect",
+        "--depth",
+        "1",
+        "--path",
+        temp_dir_path.to_str().unwrap(),
+    ]);
+
+    let expected_licenses = vec![
+        "once_cell-LICENSE-APACHE",
+        "anyhow-LICENSE-APACHE",
+        "serde-LICENSE-APACHE",
+        "spdx-LICENSE-MIT",
+        "colored-LICENSE",
+        "itertools-LICENSE-MIT",
+        "once_cell-LICENSE-MIT",
+        "toml-LICENSE-APACHE",
+        "spdx-LICENSE-APACHE",
+        "itertools-LICENSE-APACHE",
+        "indicatif-LICENSE",
+        "serde_json-LICENSE-APACHE",
+        "anyhow-LICENSE-MIT",
+        "serde-LICENSE-MIT",
+        "cargo_metadata-LICENSE-MIT",
+        "clap-LICENSE-APACHE",
+        "serde_json-LICENSE-MIT",
+        "strsim-LICENSE",
+        "clap-LICENSE-MIT",
+        "toml-LICENSE-MIT",
+    ];
+
+    assert!(output.status.success());
+    assert_eq!(expected_licenses, list_files(temp_dir_path))
 }
